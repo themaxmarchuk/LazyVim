62f86da8f65c4c163b9c212b4d3486cd9e116d42
Max Marchuk <themaxmarchuk@gmail.com>
2025-09-03 20:36:42 -0500

    drop old gdscript lsp config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -13,22 +13,22 @@ return {
       rust_analyzer = {},
       ols = { mason = false },
     },
-    setup = {
-      gdscript = function(_, opts)
-        opts.root_dir = lspconfig.util.root_pattern("project.godot")
-
-        if is_win then
-          opts.cmd = {
-            "ncat",
-            "localhost",
-            "6005",
-          }
-        end
-
-        opts.cmd = {
-          "gdfast",
-        }
-      end,
-    },
+    -- setup = {
+    --   gdscript = function(_, opts)
+    --     opts.root_dir = lspconfig.util.root_pattern("project.godot")
+    --
+    --     if is_win then
+    --       opts.cmd = {
+    --         "ncat",
+    --         "localhost",
+    --         "6005",
+    --       }
+    --     end
+    --
+    --     opts.cmd = {
+    --       "gdfast",
+    --     }
+    --   end,
+    -- },
   },
 }
